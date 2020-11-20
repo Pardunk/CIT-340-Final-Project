478be5c711eb43630525da177a30c2dbe2d9cc14
PaigeM343 <73603362+PaigeM343@users.noreply.github.com>
2020-11-20 18:56:19 -0500

    Added Main Menu
    Main menu that loads to Paige_Scene

---
+++ Final Project/Assets/P_Scripts/P_ButtonScript.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class P_ButtonScript : MonoBehaviour
+{
+    //Paige worked on the Main Menu Script
+    public void Play()
+    {
+        SceneManager.LoadScene("Paige_Scene");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+
+#endif
+
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene("Paige_Scene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("P_CreditsScene");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
+
