5126f8e81f1947f9557012558a5623aebb91d78d
Jared Mosley <mosley28@live.marshall.edu>
2020-11-20 18:53:02 -0500

    Added Scripts, Sprites, Etc.
    Added in more prototype sprites and worked on the players controls as well as created a goal script(doesnt do anything really as of now).

---
+++ Final Project/Assets/Scripts/JMcheeseGet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class JMcheeseGet : MonoBehaviour
+{
+
+
+    AudioSource victory;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+  
+        victory = GetComponent<AudioSource>();
+        victory.Play();
+
+        Debug.Log("Cheese Get");
+        // SceneManager.LoadScene("next level");
+    }
+}
