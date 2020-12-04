c4fba5639e1951f5aef94d7d76665fe9e6a800fb
PaigeM343 <73603362+PaigeM343@users.noreply.github.com>
2020-12-03 19:10:40 -0500

    Added Jared'sName to class on his scripts, made changes to Paige_Scene level design, changed sorting layers on JM box prefab, added coin credit to mainmenu
    Tested Paige_Scene with player prefab, changed JM box prefab sorting layer, added a credit to credit scene, fixed Jared's issue with script names and classes

--- Final Project/Assets/Scripts/JMPlayerMove.cs
+++ Final Project/Assets/Scripts/JMPlayerMove.cs
@@ -3,7 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class PlayerMove : MonoBehaviour
+public class JMPlayerMove : MonoBehaviour
 {
 
     public float speed = 5;
