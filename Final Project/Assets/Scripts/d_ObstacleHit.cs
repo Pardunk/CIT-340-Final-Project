a50b77582a1942ab9b47c2f57b89b61e2bce3a47
Pardunk <73765647+Pardunk@users.noreply.github.com>
2020-12-04 19:59:55 -0500

    Created d_ObstacleHit.cs

---
+++ Final Project/Assets/Scripts/d_ObstacleHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class d_ObstacleHit : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == GameObject.FindGameObjectWithTag("Player"))
+            //STUBBED: whatever we want to happen to the player when they get hit by an enemy will go here.
+            Debug.Log("Hit!");
+    }
+}
