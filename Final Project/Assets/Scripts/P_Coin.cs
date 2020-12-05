887a93c0830b8f29b6cee8ee812fdff1d3296a13
Jared Mosley <mosley28@live.marshall.edu>
2020-12-05 00:17:28 -0500

    Coin sound Attached
    attached sound to coin and edited coin script(sound not playing, need to sort that out but it is there).

--- Final Project/Assets/Scripts/P_Coin.cs
+++ Final Project/Assets/Scripts/P_Coin.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class P_Coin : MonoBehaviour
 { //Paige worked on P_Coin Script
+    //Jared Added audio soruce and audio play
+    AudioSource coin;
+
     public int coinValue = 2;
     private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.gameObject.CompareTag("Player"))
+    {   
+        if (other.gameObject.CompareTag("Player"))
         {
+            coin = GetComponent<AudioSource>();
+            coin.Play();
+
             P_CoinCounter.instance.ScoreChange(coinValue);
             
         }
