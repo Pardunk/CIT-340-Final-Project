24a78683e083c8d5d04e3e134dcb5ba2d61f7c72
Jared Mosley <mosley28@live.marshall.edu>
2020-12-05 22:39:23 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/Pardunk/CIT-352-Final-Project into main""
    This reverts commit 765ebcabf06d75242f8a4255790c148594f73558.

--- Final Project/Assets/Scripts/d_ObstacleHit.cs
+++ Final Project/Assets/Scripts/d_ObstacleHit.cs
@@ -8,7 +8,6 @@ public class d_ObstacleHit : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject == GameObject.FindGameObjectWithTag("Player"))
-            //STUBBED: whatever we want to happen to the player when they get hit by an enemy will go here.
-            Debug.Log("Hit!");
+            GameObject.FindGameObjectWithTag("Player").GetComponent<JMPlayerMove>().Respawn();
     }
 }
