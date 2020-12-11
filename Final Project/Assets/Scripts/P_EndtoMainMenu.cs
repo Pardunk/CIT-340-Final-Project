fbe2040e3eaa30fcab2331d46b52e349bb6232ad
Pardunk <73765647+Pardunk@users.noreply.github.com>
2020-12-11 06:14:38 -0500

    Added BeatLeve() Coroutine to P_EndToMainMenu

--- Final Project/Assets/Scripts/P_EndtoMainMenu.cs
+++ Final Project/Assets/Scripts/P_EndtoMainMenu.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class P_EndtoMainMenu : MonoBehaviour
 {
+    public Text winText;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        StartCoroutine(BeatLevel());
+    }
+
+    IEnumerator BeatLevel()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<JMPlayerMove>().canMove = false;
+
+        winText.text = "You Did It!";
+
+        yield return new WaitForSeconds(3);
+
         SceneManager.LoadScene("P_MainMenu");
     }
 }
